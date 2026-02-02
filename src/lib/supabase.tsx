9ab92234beb699277f8dc84dd9d5847c06a7d066
JesperTobiasAndersson <andersson.jespertobias@gmail.com>
2026-02-02 19:55:47 +0100

    vercel

--- src/lib/supabase.tsx
+++ src/lib/supabase.tsx
@@ -4,3 +4,6 @@ const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+console.log("SUPABASE_URL", process.env.EXPO_PUBLIC_SUPABASE_URL);
+console.log("HAS_ANON_KEY", !!process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY);
