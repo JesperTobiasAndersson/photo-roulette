fef055f10154b314fdfab0e7574865310ea3e02a
JesperTobiasAndersson <andersson.jespertobias@gmail.com>
2026-03-14 13:19:43 +0100

    Add Root component with HTML structure and Google AdSense integration

---
+++ app/+html.tsx
@@ -0,0 +1,29 @@
+import { ScrollViewStyleReset } from "expo-router/html";
+import type { PropsWithChildren } from "react";
+
+export default function Root({ children }: PropsWithChildren) {
+  return (
+    <html lang="sv">
+      <head>
+        <meta charSet="utf-8" />
+
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5551520118812971"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
+
+      <body>
+        <ScrollViewStyleReset />
+        {children}
+      </body>
+    </html>
+  );
+}
